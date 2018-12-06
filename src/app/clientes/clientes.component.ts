5172789699e339e131c7e84a018bc48a7b10a407
LimaIsaias <coelholimaisaias@gmail.com>
2018-12-05 22:45:05 -0200

    Criando e estilizando o primeiro componente

---
+++ src/app/clientes/clientes.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-clientes',
+  templateUrl: './clientes.component.html',
+  styleUrls: ['./clientes.component.css']
+})
+export class ClientesComponent {
+
+  constructor() { }
+  cliente = '';
+  clientes = [];
+
+  addCliente() {
+    this.clientes.push(this.cliente);
+  }
+}
